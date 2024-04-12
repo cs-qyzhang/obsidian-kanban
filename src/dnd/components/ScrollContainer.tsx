7c2a848ea09e5156f8ef9509fe8c0c65349067d0
Qiuyang Zhang <cs.qyzhang@qq.com>
2024-04-12 13:26:22 +0800

    change wheel scroll in kanban to horizontal

--- src/dnd/components/ScrollContainer.tsx
+++ src/dnd/components/ScrollContainer.tsx
@@ -2,6 +2,7 @@ import classcat from 'classcat';
 import { ComponentChildren } from 'preact';
 
 import { c } from 'src/components/helpers';
+import { useEffect } from 'preact/hooks';
 
 import { Scrollable } from './Scrollable';
 import { useStoredScrollState } from './ScrollStateContext';
@@ -24,6 +25,30 @@ export function ScrollContainer({
   index,
 }: ScrollContainerProps) {
   const { setRef, scrollRef } = useStoredScrollState(id, index);
+
+  useEffect(() => {
+    const handleWheel = event => {
+      if (event.deltaY !== 0) {
+        event.preventDefault();
+        if (scrollRef.current) {
+          scrollRef.current.scrollLeft += event.deltaY + event.deltaX;
+        }
+      }
+    };
+
+    const scrollContainer = scrollRef.current;
+    if (scrollContainer) {
+      // 添加事件监听器，不要忘记传递 `{ passive: false }`
+      scrollContainer.addEventListener('wheel', handleWheel, { passive: false });
+    }
+
+    return () => {
+      if (scrollContainer) {
+        // 组件卸载时移除事件监听器
+        scrollContainer.removeEventListener('wheel', handleWheel);
+      }
+    };
+  }, []); // 确保依赖项为空数组，这样代码只会在装载和卸载时运行
 
   return (
     <div ref={setRef} className={classcat([className, c('scroll-container')])}>
